5bc006f79b5cfc33bcbcdd7bd15af8d57161cec2
Thomas Poulsen <thomas@lha66.dk>
2022-07-03 21:36:06 +0200

    fix mutaton

--- src/dilutions.jl
+++ src/dilutions.jl
@@ -6,14 +6,25 @@ end
 concentration(solution::Acid_dilution) = concentration(solution.stock)
 concentration(acid::Union{Acid,Ion}) = acid.concentration ## Should go with definition of Acid and Ion
 
+function update(x::Acid; concentration=missing, charge=missing)
+    conc = ismissng(concentration) ? x.concentration : concentration
+    char = ismissing(charge) ? x.charge : charge
+    Acid([x.pKa;], [charge;], x.concentration)
+end
+function update(x::Ion; concentration=missing, charge=missing)
+    conc = ismissng(concentration) ? x.concentration : concentration
+    char = ismissing(charge) ? x.charge : charge
+    Ion(charge, x.concentration)
+end
+
+
 function add_solvent!(x::Acid_dilution, add_volume::Unitful.Volume)
-    ## For mutation to work both Acid and Acid_dilution need to be mutable (update concentration and volume).
-    ## First: Test how much making Acid and Ion mutable hurts performance.
+    ## fixxed mutation: only mutates here and that is not a concrete type.
     new_volume = add_volume + x.volume
     new_concentration = concentration(x)* x.volume / new_volume
-    x.stock.concentration = new_concentration
+    x.stock = update(x; concentration = new_concentration)
     x.volume = new_volume
-    new_volume
+    (;new_volume)
 end
 
 function final_volume!(x::Acid_dilution, final_volume::Unitful.Volume)
@@ -21,8 +32,9 @@ function final_volume!(x::Acid_dilution, final_volume::Unitful.Volume)
     added_volume = final_volume - x.volume
     @info "Volume to add: $(added_volume)"
     x.volume = final_volume
-    x.concentration = concentration(x)*x.volume/final_volume
-    added_volume
+    new_concentration = concentration(x)*x.volume/final_volume
+    x.stock = update(x,concentration= new_concentration)
+    (;added_volume)
 end
 
 function normalize!(x::Acid_dilution, final_concentration)
